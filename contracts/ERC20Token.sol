3637fb7fab1b8d0b595895b5bd7cc7c8e852ecb4
Frozen <xrdavies@gmail.com>
2021-05-03 00:17:15 +0800

    first code

---
+++ contracts/ERC20Token.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract ERC20Token is ERC20 {
+    constructor(uint256 initialSupply) ERC20("TEST Token", "TTT") {
+        _mint(msg.sender, initialSupply);
+    }
+}
---
+++ contracts/MultiSender.sol
@@ -0,0 +1,4 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
