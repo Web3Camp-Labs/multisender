0ff4e2604c0a58b8fda85e7447143703a6327945
Frozen <xrdavies@gmail.com>
2021-08-23 16:05:11 +0800

    add batchSendEther

--- test/sender.js
+++ test/sender.js
@@ -23,10 +23,9 @@ contract('MultiSender Contract', accounts => {
         assert(balance.eq(SUPPLY), '1B wasn\'t in the first account');
     });
 
-    it('multi send', async () => {
+    it('multi send token', async () => {
         const balance = await token.balanceOf(accounts[0]);
         assert(balance.eq(SUPPLY), '1B wasn\'t in the first account');
-
 
         let amounts = [
             UNIT.mul(new BN('1000'))
@@ -53,7 +52,7 @@ contract('MultiSender Contract', accounts => {
 
         let totalAmount = new BN('0');
 
-        for(const elem of amounts) {
+        for (const elem of amounts) {
             totalAmount = totalAmount.add(elem);
         }
 
@@ -71,6 +70,76 @@ contract('MultiSender Contract', accounts => {
 
             const balance = await token.balanceOf(receiver);
             assert(balance.eq(amount), 'wrong balance');
+
+            index += 1;
+        }
+
+    });
+
+    it('multi send eth', async () => {
+
+        let amounts = [
+            web3.utils.toWei('1')
+            , web3.utils.toWei('2')
+            , web3.utils.toWei('3')
+            , web3.utils.toWei('4')
+            , web3.utils.toWei('5')
+            , web3.utils.toWei('6')
+            , web3.utils.toWei('7')
+            , web3.utils.toWei('8')
+            , web3.utils.toWei('9')];
+
+        // let amounts = [
+        //     UNIT.mul(new BN('0.1'))
+        //     , UNIT.mul(new BN('0.2'))
+        //     , UNIT.mul(new BN('0.3'))
+        //     , UNIT.mul(new BN('0.4'))
+        //     , UNIT.mul(new BN('0.5'))
+        //     , UNIT.mul(new BN('0.6'))
+        //     , UNIT.mul(new BN('0.7'))
+        //     , UNIT.mul(new BN('0.8'))
+        //     , UNIT.mul(new BN('0.9'))];
+
+        let receivers = [
+            accounts[1]
+            , accounts[2]
+            , accounts[3]
+            , accounts[4]
+            , accounts[5]
+            , accounts[6]
+            , accounts[7]
+            , accounts[8]
+            , accounts[9]
+        ]
+
+        let totalAmount = new BN('0');
+
+        for (const elem of amounts) {
+            totalAmount = totalAmount.add(new BN(elem));
+        }
+
+        console.log("Total amount to send: ", totalAmount);
+
+        let s = await web3.eth.getBalance(accounts[0]);
+        let balance = web3.utils.fromWei(s);
+        console.log("Current balance: ", balance);
+
+        const res = await sender.batchSendEther(receivers, amounts, {
+            from: accounts[0],
+            value: web3.utils.toHex(totalAmount),
+        });
+
+
+        console.log("result : ", res);
+
+        let index = 0;
+        for (const elem of receivers) {
+            let amount = amounts[index];
+            let receiver = receivers[index];
+            let s = await web3.eth.getBalance(receiver);
+            let balance = web3.utils.fromWei(s);
+            // assert(balance.eq(amount), 'wrong balance');
+            console.log(`Address: ${receiver}, amount: ${amount}, balance: ${balance}`);
 
             index += 1;
         }
