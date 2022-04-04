b9252d8aeb370ac619f5888d5310a78cc3c615cc
wamma9527 <117485655@qq.com>
2022-04-04 10:47:57 +0800

    add navigation

--- front-end/pages/index.tsx
+++ front-end/pages/index.tsx
@@ -5,6 +5,7 @@ import styled from "styled-components";
 import HeaderTop from "./components/headTop";
 // import BatchQuery from "./components/batchQuery";
 import FooterBox from "./components/footerBox";
+import {Display,FileEarmarkCode,Check2Square} from "react-bootstrap-icons"
 const MainBox = styled.div`
     display: flex;
   flex-grow: 1;
@@ -25,6 +26,44 @@ const CardBox = styled(Card)`
   border-radius: 6px;
 `
 
+const NavBox = styled.div`
+    height: 90px;
+    margin: 40px ;
+    .bg{
+      border-bottom: 1px solid #eee;
+      height: 40px;
+    }
+    .box{
+      width: 100%;
+      display: flex;
+      justify-content: space-between;
+      height: 80px;
+    }
+  li{
+    background: #fff;
+    padding: 0 40px;
+    &>div{
+      width: 80px;
+      height: 80px;
+      border-radius: 80px;
+      box-shadow: 0 0 5px #eee;
+      background: #f8f8f8;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      color: #cccccc;
+      font-size: 16px;
+    }
+    &.active{
+      &>div{
+       background: #fff;
+        box-shadow: 0 0 10px rgba(128,0,128,0.3);
+        color: purple;
+      }
+    }
+  }
+`
+
 
 const Home: NextPage = () => {
   return (
@@ -41,6 +80,23 @@ const Home: NextPage = () => {
               <BgBox>
                   <Row>
                       <Col md={12} xs={12}>
+                          <NavBox>
+                              <div className="bg">
+                                  <ul className="box">
+                                      <li className="active">
+                                          <div>
+                                              <FileEarmarkCode />
+                                          </div>
+                                      </li>
+                                      <li >
+                                          <div>
+                                              <Check2Square />
+                                          </div>
+                                      </li>
+                                      <li ><div> <Display /></div></li>
+                                  </ul>
+                              </div>
+                          </NavBox>
                           <CardBox body>
                                 {/* <BatchQuery /> */}
                           </CardBox>
