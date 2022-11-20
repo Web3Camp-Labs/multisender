162775e2b200f830c50c10e4d576a32302a91c31
throwaway <7518647+wendychaung@users.noreply.github.com>
2022-11-20 16:34:29 +0800

    Update _app.tsx

--- front-end/pages/_app.tsx
+++ front-end/pages/_app.tsx
@@ -9,7 +9,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     <Head>
       <title>Multisender</title>
       <meta name="description" content="It supports sending ERC20 tokens and ETH!" />
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href="/multisender/favicon.ico" />
     </Head>
     <ContextProvider>
       <Component {...pageProps} />
